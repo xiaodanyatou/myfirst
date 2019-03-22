1056b8424575b659b50977241c1de5c2dd1bf982
单红丽 <shanhongli@shanhonglideMacBook-Pro.local>
2019-03-22 12:18:26 +0800

    99

--- firstVue/src/main.js
+++ firstVue/src/main.js
@@ -2,32 +2,33 @@ import Vue from 'vue'
 
 import Cookies from 'js-cookie'
 
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
-import '@/styles/index.scss'
+import '@/styles/index.scss' // global css
 
 import App from './App'
 import store from './store'
 import router from './router'
 
-import i18n from './lang'
-import './icons'
-import './errorLog'
-import './permission'
-import './mock'
+import i18n from './lang' // Internationalization
+import './icons' // icon
+import './errorLog' // error log
+import './permission' // permission control
+import './mock' // simulation data
 
-import * as filters from './filters'
-
+import * as filters from './filters' // global filters
 
 Vue.use(Element, {
-  size: Cookies.get('size') || 'medium',  //set element-ui default size
+  size: Cookies.get('size') || 'medium', // set element-ui default size
   i18n: (key, value) => i18n.t(key, value)
 })
 
-//register global utility filters.
+// register global utility filters.
 Object.keys(filters).forEach(key => {
-  Vue.filters(key, filters[key])
+  Vue.filter(key, filters[key])
 })
 
 Vue.config.productionTip = false
@@ -39,5 +40,3 @@ new Vue({
   i18n,
   render: h => h(App)
 })
-
-
