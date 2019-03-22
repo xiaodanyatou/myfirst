04850e5e21b92730481e2277189f3cbce3981b49
单红丽 <shanhongli@shanhonglideMacBook-Pro.local>
2019-03-22 11:47:25 +0800

    8

--- firstVue/src/permission.js
+++ firstVue/src/permission.js
@@ -5,6 +5,7 @@ import NProgress from 'nprogress' // progress bar
 import 'nprogress/nprogress.css'// progress bar style
 import { getToken } from '@/utils/auth' // getToken from cookie
 
+
 NProgress.configure({ showSpinner: false })// NProgress Configuration
 
 // permission judge function
